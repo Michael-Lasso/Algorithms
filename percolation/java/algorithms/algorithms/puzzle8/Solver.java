00136abba381d573eefb3eeddcfd9c6c3e61a333
Michael Lasso <Michael_Lasso@cable.comcast.com>
2015-05-18 12:36:27 -0400

    Works, but could use some refactoring

--- percolation/java/algorithms/algorithms/puzzle8/Solver.java
+++ percolation/java/algorithms/algorithms/puzzle8/Solver.java
@@ -17,41 +17,19 @@ public class Solver {
 	private void compute(Board initial) {
 		MinPQ<Board> p = new MinPQ<Board>();
 		p.insert(initial);
-		System.out.println("+++Initial board+++");
-		initial.test();
-		Board board = null;
+		Board board = initial;
 		p.insert(initial);
 		int moves = 0;
-		for (int i = 0; i < 6; i++) {
+		while (!board.isGoal()) {
 			board = p.delMin();
 			board.setPriotity(moves);
 			for (Board b : board.neighbors()) {
-
-				System.out.println("Added");
-				b.test();
 				p.insert(b);
-
 			}
-			System.out.println("+++Last board+++");
-		
-			System.out.println("Iteration: " + i);
+			board.test();
+			System.out.println("Iteration: " + (int) moves / 100);
 			moves += 100;
 		}
-	}
-
-	// is the initial board solvable?
-	public boolean isSolvable() {
-		return false;
-	}
-
-	// min number of moves to solve initial board; -1 if unsolvable
-	public int moves() {
-		return moves;
-	}
-
-	// sequence of boards in a shortest solution; null if unsolvable
-	public Iterable<Board> solution() {
-		return null;
 	}
 
 	// solve a slider puzzle (given below)
@@ -66,15 +44,8 @@ public class Solver {
 				blocks[i][j] = in.readInt();
 		Board initial = new Board(blocks);
 
-		// solve the puzzle
 		Solver solver = new Solver(initial);
 
-		/*
-		 * // print solution to standard output if (!solver.isSolvable()) {
-		 * StdOut.println("No solution possible"); } else {
-		 * StdOut.println("Minimum number of moves = " + solver.moves()); for
-		 * (Board board : solver.solution()) { StdOut.println(board); } }
-		 */
 	}
 
 }
